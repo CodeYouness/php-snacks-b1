ec9510dd6f10966cf0ba8cef53f78067f63a8fdb
CodeYouness <ylijassi@gmail.com>
2024-06-26 13:01:36 +0200

    fixed validation of num

--- Snack2/login.php
+++ Snack2/login.php
@@ -2,7 +2,7 @@
 
 $agenum = (float)$_GET['name'];
 
-if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@') and $agenum) {
+if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@') and is_nan($_GET['name'])) {
     echo 'DAJE ROMA DAJE';
 } else {
     echo 'DAJE LAZIO DAJE';
