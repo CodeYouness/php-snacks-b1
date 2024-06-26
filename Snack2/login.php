780a98972d0c9b8697de334674c60cf48e6eec94
CodeYouness <ylijassi@gmail.com>
2024-06-26 12:56:30 +0200

    added else

--- Snack2/login.php
+++ Snack2/login.php
@@ -1,5 +1,7 @@
 <?php
 
 if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@')) {
-    echo 'daje';
+    echo 'DAJE ROMA DAJE';
+} else {
+    echo 'DAJE LAZIO DAJE';
 }
