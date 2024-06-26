f9f80d5982d2d7548ca73be6b8360ee7e03a20af
CodeYouness <ylijassi@gmail.com>
2024-06-26 12:30:20 +0200

    added other 2 array

--- Snack1/index.php
+++ Snack1/index.php
@@ -5,12 +5,31 @@ $basket = [
         "team2" => "Borgonova",
         "point1" => rand(1, 100),
         "point2" => rand(1, 100),
-    ]
+    ],
+    [
+        "team1" => "Africa",
+        "team2" => "Europa",
+        "point1" => rand(1, 100),
+        "point2" => rand(1, 100),
+    ],
+    [
+        "team1" => "Battellovapore",
+        "team2" => "SugoAlPesto",
+        "point1" => rand(1, 100),
+        "point2" => rand(1, 100),
+    ],
+    [
+        "team1" => "Pennette",
+        "team2" => "ConSugo",
+        "point1" => rand(1, 100),
+        "point2" => rand(1, 100),
+    ],
 ];
 
 foreach ($basket as $key => $value) {
     foreach ($basket[$key] as $key => $value) {
         echo $key;
+        echo "</br>";
         echo $value;
     }
 }
