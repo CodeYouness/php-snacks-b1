45db8371d15eb6825521e8dffac62ade0e03dbe9
CodeYouness <ylijassi@gmail.com>
2024-06-26 12:58:36 +0200

    added validation of number

--- Snack2/login.php
+++ Snack2/login.php
@@ -1,6 +1,8 @@
 <?php
 
-if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@')) {
+$agenum = (float)$_GET['name'];
+
+if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@') and $agenum) {
     echo 'DAJE ROMA DAJE';
 } else {
     echo 'DAJE LAZIO DAJE';
