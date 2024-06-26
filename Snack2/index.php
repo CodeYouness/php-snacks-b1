45c640de00562b76a1c362fa82a37487ea7d9ff8
CodeYouness <ylijassi@gmail.com>
2024-06-26 12:45:54 +0200

    added form

---
+++ Snack2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <form action="./login.php" method="get">
+            <input type="text" name="name" id="name">
+            <input type="text" name="email" id="email">
+            <input type="text" name="age" id="age">
+        </form>
+    </main>
+</body>
+
+</html>
