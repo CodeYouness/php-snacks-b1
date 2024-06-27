280a2cb150d1e8c91909f8f8f79b380d0d051fbd
CodeYouness <ylijassi@gmail.com>
2024-06-27 12:59:33 +0200

    filtered array

--- Snack8/index.php
+++ Snack8/index.php
@@ -1,7 +1,19 @@
 <?php
 include_once __DIR__ . "/animals.php";
 
+$fish = [];
+$mammals = [];
+$reptiles = [];
+
 foreach ($animals as $value) {
 
-    $fish = array_filter($animals, $animals[]);
+    if ($value['class'] == 'fish') {
+        $fish[] = $value;
+    } elseif ($value['class'] == 'mammals') {
+        $mammals[] = $value;
+    } elseif ($value['class'] == 'reptiles') {
+        $reptiles[] = $value;
+    }
 }
+
+var_dump($fish);
