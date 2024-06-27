fb556376dddcf5dc7a8d385b93d43b5666a7ffa6
CodeYouness <ylijassi@gmail.com>
2024-06-27 12:35:53 +0200

    added list for each student

--- Snack7/index.php
+++ Snack7/index.php
@@ -1,3 +1,13 @@
 <?php
 include __DIR__ . "/students.php";
 var_dump($students);
+?>
+
+
+<?php foreach ($students as $student) { ?>
+    <h1><?php echo $student['name'] . ' ' . $student['surname'] ?></h1>
+    <ul>
+
+    </ul>
+
+<?php } ?>
