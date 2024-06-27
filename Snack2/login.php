183ded937ed4e72b8a6cf9cbf5af69b7836207fa
CodeYouness <ylijassi@gmail.com>
2024-06-27 09:40:42 +0200

    fixed snack 2

--- Snack2/login.php
+++ Snack2/login.php
@@ -2,7 +2,7 @@
 $agenum = isset($_GET['age']);
 if ($agenum)
 
-    if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@') and !$agenum) {
+    if (strlen($_GET['name'] > 3) and str_contains($_GET['email'], '.') and str_contains($_GET['email'], '@') and !is_numeric($_GET('age'))) {
         echo 'DAJE ROMA DAJE';
     } else {
         echo 'DAJE LAZIO DAJE';
